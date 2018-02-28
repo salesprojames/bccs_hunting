a3991798f4386bd1741b306538e81004672db09b
TimothyC04 <35073995+TimothyC04@users.noreply.github.com>
2018-02-28 12:12:56 -0500

    First commit for gun movement

--- move_gun.js
+++ move_gun.js
@@ -1,8 +1,52 @@
+var canvas = document.getElementById("canvas");
+var context = canvas.getContext("2d");
+
+function drawBoxGun(){
+	context.beginPath();
+	context.arc(600, 200, 50, .5 * Math.PI, 1.5 * Math.PI);
+	context.closePath();
+	context.fillStyle = "black";
+	context.fill();
+	context.strokeStyle = "black";
+
+	//context.rotate(20 * Math.PI / 180);
+	context.fillRect(490, 190, 100, 20);
+	}
+
+	
+
+var gun = {
+	x: 490,
+	y: 200,
+	movement: null,
+	dir: null
+}
+
 function moveGunUp(){
   //Functionality goes here.  See keyclick.js
-  console.log("up")
+  gun.dir = "up"
+  
+  	
+  	if (gun.y >= 130){
+ 	 gun.y -= 1
+ 		if (gun.movement == "negative"){
+ 		 gun.x -= .5
+ 		} else {
+ 		 gun.x += .5
+ 		}
+  	}
+ 	
 }
 
 function moveGunDown(){
-	console.log("down")
+	gun.dir = "down"
+	 
+	 if (gun.y <= 270){
+ 	 	gun.y += 1
+ 	 	if (gun.movement == "negative"){
+ 		 gun.x -= .5
+ 		} else {
+ 		 gun.x += .5
+ 		}
+ 	 }
 }
